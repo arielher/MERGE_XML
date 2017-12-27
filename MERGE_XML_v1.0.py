52d7baf991745ed8b170f7fb826f57a332798ccf
arielher <34895047+arielher@users.noreply.github.com>
2017-12-27 20:33:41 +0200

    Update MERGE_XML_v1.0.py

--- MERGE_XML_v1.0.py
+++ MERGE_XML_v1.0.py
@@ -10,37 +10,25 @@ def run(files):
         os.remove(out_file)
     except OSError:
         pass
-    
-    #xml_files = [file + ".xml" for file in files]
-    #xml_files = glob.glob(files +"/*.xml")
-    #print(files)
     xml_element_tree = None
     for xml_file in glob.glob("*.xml"):
-    #for xml_file in files:
         print(xml_file)
         data = ElementTree.parse(xml_file).getroot()
-        # print ElementTree.tostring(data)
-        #print (data)
-        #print (xml_file)
         for result in data.iter('GlassList'):
             newKid = ElementTree.Element('File_Name', name=xml_file)
             result.insert(0,newKid);
         for result in data.iter('DocumentElement'):
-            #print("D0")
             if xml_element_tree is None:
-                #print ("D1")
                 xml_element_tree = data 
-                #insertion_point = xml_element_tree.findall("./DocumentElement")[0]
                 insertion_point = data
-                #print ("insertion_point" + insertion_point)
             else:
-                #print ("D2")
                 insertion_point.extend(result)
     if xml_element_tree is not None:
         with open(out_file,'wb') as f:
             f.write(ElementTree.tostring(xml_element_tree))
     else:
-        print("2")
+        print("Error: file not created")
+
 
 tmpDir = filedialog.askdirectory()
 os.chdir(tmpDir)
